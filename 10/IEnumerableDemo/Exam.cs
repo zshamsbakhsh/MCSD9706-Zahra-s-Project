6f9e2eef804c435d02c1bae5663fb6dfad0eb94f
Zshamsbakhsh <zshamsbakhsh@gmail.com>
2018-10-06 15:01:59 +0330

    Update.Session10

--- 10/IEnumerableDemo/Exam.cs
+++ 10/IEnumerableDemo/Exam.cs
@@ -38,31 +38,39 @@ namespace IEnumerableDemo
 
         public bool MoveNext()
         {
+
+
+            // currentIndex = (new Random()).Next(0,20) ;
+
             //currentIndex += 2;
-            int[] randomNumbers = new int[5];
+            //int[] randomNumbers = new int[5];
+            //Random r = new Random();
+            //for (int i = 0; i < randomNumbers.Length; /*i++*/)
+            //{
+            //    var randomNumber = r.Next(1, 20);
+            //    bool isUnique = true;
+            //    for (int j = 0; j < i; j++)
+            //    {
+            //        if (randomNumbers[j] == randomNumber)
+            //        {
+            //            isUnique = false;
+            //            break;
+            //        }
+
+            //    }
+            //    if (isUnique)
+            //        randomNumbers[i++] = randomNumber;
+            //}
+
+            //for (int i=0; i<5; i++)
+            //{
+            //    currentIndex = randomNumbers[i];
+
+            //}
+
+
             Random r = new Random();
-            for (int i = 0; i < randomNumbers.Length; /*i++*/)
-            {
-                var randomNumber = r.Next(1, 20);
-                bool isUnique = true;
-                for (int j = 0; j < i; j++)
-                {
-                    if (randomNumbers[j] == randomNumber)
-                    {
-                        isUnique = false;
-                        break;
-                    }
-
-                }
-                if (isUnique)
-                    randomNumbers[i++] = randomNumber;
-            }
-
-            for (int i=0; i<5; i++)
-            {
-                currentIndex = randomNumbers[i];
-                
-            }
+            currentIndex = r.Next(1, 20);
 
             return currentIndex<questions.Length;
         }

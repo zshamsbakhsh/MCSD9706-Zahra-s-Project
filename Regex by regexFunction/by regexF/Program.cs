78a047975287aa1cb868f0d838944665287ef878
Zshamsbakhsh <zshamsbakhsh@gmail.com>
2018-10-10 15:41:13 +0330

    By F

---
+++ Regex by regexFunction/by regexF/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace by_regexF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                //Console.Write("Pattern:");
+                //var pattern = Console.ReadLine();
+                //if (pattern == "exit")
+                //    break;
+                Regex re = new Regex(@"^[0-9][0-9][0-9][0-9][01][0-9][1-3][09]");
+                while (true)
+                {
+                    Console.Write("Text:");
+                    var text = Console.ReadLine();
+                    if (text == "exit")
+                        break;
+                    var isMatch = re.IsMatch(text);
+                    if (isMatch)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Match!");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Not Match!");
+                    }
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+
+        }
+    
+    }
+}
